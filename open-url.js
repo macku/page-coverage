4e86ba9d020cd7a12a16f2fae74f9da9d2202ac1
Maciej Adamczak <me@maciejadamczak.net>
2018-03-12 13:18:41 +1100

    Support JSON output for error

--- open-url.js
+++ open-url.js
@@ -14,16 +14,18 @@ module.exports = async function openUrl(url, {
   cookies,
   json
 }) {
-  if (!validUrl.isWebUri(url)) {
-    console.log(`The passed URL "${url}" doesn't look a like valid address`);
-    process.exit(1);
-  }
-
   let browser;
   const verboseOutput = !json;
   const consoleOutput = getConsole(verboseOutput);
+  const consoleError = error => (
+    json ? console.log(JSON.stringify({ error })) : console.log(`Error: ${error}`)
+  );
 
   try {
+    if (!validUrl.isWebUri(url)) {
+      throw new TypeError(`The passed URL '${url}' doesn't look a like valid address`);
+    }
+
     consoleOutput('Collecting the JS and CSS coverage usage. Please wait...');
 
     browser = await openBrowser({ headless });
@@ -52,7 +54,7 @@ module.exports = async function openUrl(url, {
     process.stdout.write('\033c');
     console.log(formatCoverage(url, pageUrl, coverage, json));
   } catch (e) {
-    console.log(`[Error] ${e.message}`);
+    consoleError(e.message);
     process.exit(1);
   } finally {
     if (browser) {
